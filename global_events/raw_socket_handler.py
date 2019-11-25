969107448ca9351e62d7807e366e17b9a359a478
PueChoc <pue@paradoxical.pw>
2019-11-25 22:41:14 +1100

    fix(raw_socket_handler): Don't engage the raw event handler when the pipes don't exist.

--- global_events/raw_socket_handler.py
+++ global_events/raw_socket_handler.py
@@ -1,18 +1,25 @@
 import os
 
+app = os.getcwd().split(os.sep)[-1]
+pipefile = "/home/paradox/pipe/"+app
+
+existence = os.path.exists(pipefile)
+
 cest_une_pipe = None
 
 
 async def handle_raw_socket(bot, msg):
+    if not existence:
+        return
+
     global cest_une_pipe
-    if (cest_une_pipe == None):
-        app = os.getcwd().split(os.sep)[-1]
-        cest_une_pipe = open("/home/paradox/pipe/"+app,"w")
-    if isinstance(msg,str):
+    if cest_une_pipe is None:
+        cest_une_pipe = open(pipefile, "w")
+    if isinstance(msg, str):
         cest_une_pipe.write(msg)
         cest_une_pipe.write('\n')
 
 
 def load_into(bot):
-    if os.path.exists("/home/paradox"):
+    if existence:
         bot.add_after_event("socket_raw_receive", handle_raw_socket, priority=5)
