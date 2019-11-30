8a370ea3329683ca7e907daa04536c5af125db43
PueChoc <pue@paradoxical.pw>
2019-12-01 03:07:34 +1100

    cleanup(User utils): Removed obsolete timezone command.
    The `timezone` command is made obsolete by the `time --at` feature.

--- modules/User_Utils/extra_util_cmds.py
+++ modules/User_Utils/extra_util_cmds.py
@@ -1,8 +1,6 @@
 import discord
 from datetime import datetime
 from pytz import timezone
-import pytz
-import iso8601
 import aiohttp
 import string
 
@@ -253,32 +251,6 @@ async def cmd_piggybank(ctx):
         await ctx.reply(msg + "```", dm=True)
     else:
         await ctx.reply("Usage: {}piggybank [+|- <amount>] | [list] | [goal <amount>|none]".format(ctx.used_prefix))
-
-
-@cmds.cmd("timezone",
-          category="Utility",
-          short_help="Searches the timezone list",
-          aliases=["tz", "timein"])
-async def cmd_timezone(ctx):
-    """
-    Usage:
-        {prefix}timezone <partial>
-    Description:
-        Searches for <partial> amongst the available timezones and shows you the current time in each!
-    """
-    timestr = '%I:%M %p'
-    tzlist = [(tz, iso8601.parse_date(datetime.now().isoformat()).astimezone(timezone(tz)).strftime(timestr)) for tz in pytz.all_timezones]
-    if ctx.arg_str:
-        tzlist = [tzpair for tzpair in tzlist if (ctx.arg_str.lower() in tzpair[0].lower()) or (ctx.arg_str.lower() in tzpair[1].lower())]
-    if not tzlist:
-        await ctx.reply("No timezones were found matching these criteria!")
-        return
-
-    tz_blocks = [tzlist[i:i + 20] for i in range(0, len(tzlist), 20)]
-    max_block_lens = [len(max(list(zip(*tz_block))[0], key=len)) for tz_block in tz_blocks]
-    block_strs = [["{0[0]:^{max_len}} {0[1]:^10}".format(tzpair, max_len=max_block_lens[i]) for tzpair in tzblock] for i, tzblock in enumerate(tz_blocks)]
-    tz_pages = ["```\n{}\n```".format("\n".join(block)) for block in block_strs]
-    await ctx.pager(tz_pages)
 
 
 @cmds.cmd(name="colour",
