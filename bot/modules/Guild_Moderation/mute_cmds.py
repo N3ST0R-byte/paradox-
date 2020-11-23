525aae047ce9c9ff1d8b2e3b39edff0738746edd
Puerosola <pue@puerosola.pw>
2020-11-23 15:12:04 +0200

    rewrite: Bugfixes in `mute`.

--- bot/modules/Guild_Moderation/mute_cmds.py
+++ bot/modules/Guild_Moderation/mute_cmds.py
@@ -12,7 +12,7 @@ from . import mute_config  # noqa
 # TODO: Muterole creation
 @module.cmd("mute",
             desc="Silence a misbehaving user for a specified amount of time.",
-            flags=['r', 'f', 't=='],
+            flags=['r==', 'f', 't=='],
             handle_edits=False)
 @guild_moderator()
 async def cmd_mute(ctx, flags):
@@ -32,6 +32,9 @@ async def cmd_mute(ctx, flags):
     Examples``:
         {prefix}mute {ctx.author} -t 1d
     """
+    if not ctx.args:
+        return await ctx.error_reply("No arguments given, nothing to do.")
+
     # Give deprecation warning for -f
     if flags['f']:
         return await ctx.error_reply("The `f(ake)` flag is deprecated and will be removed in a future update.")
@@ -89,12 +92,12 @@ async def cmd_mute(ctx, flags):
         if len(muted) == 1:
             reports.append("Muted `{}`.".format(muted[0]))
         else:
-            reports.append("Muted: `{}`.".format('`, `'.join(muted)))
+            reports.append("Muted: `{}`.".format('`, `'.join(str(u) for u in muted)))
     if failed:
         if len(failed) == 1:
             reports.append("Failed to mute `{}`.".format(failed[0]))
         else:
-            reports.append("Failed to mute: `{}`.".format('`, `'.join(failed)))
+            reports.append("Failed to mute: `{}`.".format('`, `'.join(str(u) for u in failed)))
 
     await ctx.reply('\n'.join(reports))
 """
@@ -117,7 +120,7 @@ async def cmd_mute(ctx, flags):
 
 @module.cmd("unmute",
             desc="Unmute muted users.",
-            flags=['r', 'f'],
+            flags=['r==', 'f'],
             handle_edits=False)
 @guild_moderator()
 async def cmd_unmute(ctx, flags):
@@ -136,6 +139,9 @@ async def cmd_unmute(ctx, flags):
     Examples``:
         {prefix}unmute {ctx.author}
     """
+    if not ctx.args:
+        return await ctx.error_reply("No arguments given, nothing to do.")
+
     # Give deprecation warning for -f
     if flags['f']:
         return await ctx.error_reply("The `f(ake)` flag is deprecated and will be removed in a future update.")
@@ -174,12 +180,12 @@ async def cmd_unmute(ctx, flags):
         if len(muted) == 1:
             reports.append("UnMuted `{}`.".format(muted[0]))
         else:
-            reports.append("UnMuted: `{}`.".format('`, `'.join(muted)))
+            reports.append("UnMuted: `{}`.".format('`, `'.join(str(u) for u in muted)))
     if failed:
         if len(failed) == 1:
             reports.append("Failed to unmute `{}`.".format(failed[0]))
         else:
-            reports.append("Failed to unmute: `{}`.".format('`, `'.join(failed)))
+            reports.append("Failed to unmute: `{}`.".format('`, `'.join(str(u) for u in failed)))
 
     await ctx.reply('\n'.join(reports))
 """
@@ -195,5 +201,3 @@ async def cmd_unmute(ctx, flags):
 # We get a bunch of exceptions and return values here
 # Use these to form the return string, dictating the mutes which succeeded and failed
 """
-
-
