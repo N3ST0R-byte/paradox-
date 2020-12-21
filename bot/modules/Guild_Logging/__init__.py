410bed35e79a90c3a8b50e2c9e8a7f5c793fd3fe
Puerosola <pue@puerosola.pw>
2020-12-21 23:31:05 +0200

    rewrite (Guild Logging): User update system.

--- bot/modules/Guild_Logging/__init__.py
+++ bot/modules/Guild_Logging/__init__.py
@@ -1,1 +1,2 @@
 from . import user_traffic
+from . import user_updates
--- bot/modules/Guild_Logging/user_updates.py
+++ bot/modules/Guild_Logging/user_updates.py
@@ -1,70 +1,231 @@
-import discord
+import logging
+import asyncio
 from datetime import datetime
+from enum import Enum
 
-from settings import GuildSetting, Channel, ColumnData
+import discord
+
+from settings import GuildSetting, SettingList, Channel, MemberList, IntegerEnum, ColumnData, ListData
 from registry import tableInterface, schema_generator, Column, ColumnType
 
 from wards import guild_manager
+from utils.lib import prop_tabulate
 
 from .module import guild_logging_module as module
 
 
-async def log_member_update(bot, before, after):
-    userlog = await bot.data.servers.get(before.server.id, "userlog_ch")
+class UserLogEvent(Enum):
+    USERNAME = 0
+    NICKNAME = 1
+    AVATAR = 2
+    ROLES = 3
+
+
+async def member_update_handler(client, before, after, from_user=False, guild=None):
+    # Check the event is one we we can handle
+    if not (before.name != after.name
+            or (not from_user and before.nick != after.nick)
+            or before.avatar_url != after.avatar_url
+            or (not from_user and before.roles != after.roles)):
+        return
+
+    # Get the various guild settings, with appropriate return conditions
+    guild = after.guild if not from_user else guild
+    userlog = client.guild_config.userlog.get(client, guild.id).value
     if not userlog:
         return
-    userlog = before.server.get_channel(userlog)
-    if not userlog:
+
+    userlog_events = client.guild_config.userlog_events.get(client, guild.id).value
+    if not userlog_events:
         return
 
-    log_ignore = await bot.data.servers.get(before.server.id, "userlog_ignore")
-    if log_ignore and (before.id in log_ignore):
+    userlog_ignore = client.guild_config.userlog_ignores.get(client, guild.id).value
+    if userlog_ignore and before in userlog_ignore:
         return
 
-    events = await bot.data.servers.get(before.server.id, "userlog_events")
-
+    # Build the description
     desc_lines = []
-    image_url = None
-    if (events is None or "username" in events) and before.name != after.name:
-        desc_lines.append("{}#{} ({}) changed their username.".format(after.name, after.discriminator, after.mention))
-        desc_lines.append("`Before:` {}".format(before.name))
-        desc_lines.append("`After:` {}".format(after.name))
+    image = None
 
-    if (events is None or "nickname" in events) and before.nick != after.nick:
-        desc_lines.append("{}#{} ({}) changed their nickname.".format(after.name, after.discriminator, after.mention))
-        desc_lines.append("`Before:` {}".format(before.nick))
-        desc_lines.append("`After:` {}".format(after.nick))
+    if before.name != after.name and UserLogEvent.USERNAME in userlog_events:
+        # Handle name changes
+        desc_lines.append(
+            "**Username updated!**\n"
+            "`Before:` {}\n"
+            "`After:` {}\n".format(before.name, after.name)
+        )
 
-    if (events is None or "avatar" in events) and before.avatar_url != after.avatar_url:
-        desc_lines.append("{}#{} ({}) changed their avatar.".format(after.name, after.discriminator, after.mention))
-        old_av = "[Old Avatar]({})".format(before.avatar_url) if before.avatar_url else "None"
-        new_av = "[New Avatar]({})".format(after.avatar_url) if after.avatar_url else "None"
-        desc_lines.append("`Before:` {}".format(old_av))
-        desc_lines.append("`After:` {}".format(new_av))
-        image_url = after.avatar_url if after.avatar_url else None
+    if not from_user and before.nick != after.nick and UserLogEvent.NICKNAME in userlog_events:
+        # Handle nickname changes
+        desc_lines.append(
+            "**Nickname updated!**\n"
+            "`Before:` {}\n"
+            "`After:` {}\n".format(before.nick, after.nick)
+        )
 
-    if (events is None or "roles" in events) and before.roles != after.roles:
+    if before.avatar_url != after.avatar_url and UserLogEvent.AVATAR in userlog_events:
+        # Handle avatar changes
+        desc_lines.append(
+            "**Avatar updated!**\n"
+            "`Before:` [Old Avatar]({})\n"
+            "`After:` [New Avatar]({})\n".format(before.avatar_url, after.avatar_url)
+        )
+        image = after.avatar_url if after.avatar_url else None
+
+    if not from_user and before.roles != after.roles and UserLogEvent.ROLES in userlog_events:
+        # Handle role changes
         before_roles = [role.name for role in before.roles]
         after_roles = [role.name for role in after.roles]
         added_roles = [role for role in after_roles if role not in before_roles]
         removed_roles = [role for role in before_roles if role not in after_roles]
-        desc_lines.append("The roles of {}#{} ({}) were modified.".format(after.name, after.discriminator, after.mention))
+        desc_lines.append("**Roles updated!**")
         if added_roles:
             desc_lines.append("Added roles `{}`".format("`, `".join(added_roles)))
         if removed_roles:
             desc_lines.append("Removed roles `{}`".format("`, `".join(removed_roles)))
 
+    # Return if we have somehow ended up with an empty description
     if not desc_lines:
         return
 
-    description = "\n".join(desc_lines)
+    # Build log embed
+    description = "{}\n{}".format(after.mention, "\n".join(desc_lines))
     colour = (after.colour if after.colour.value else discord.Colour.light_grey())
 
     embed = discord.Embed(color=colour, description=description, timestamp=datetime.now())
-    if image_url:
-        embed.set_thumbnail(url=image_url)
-    await bot.send_message(userlog, embed=embed)
+    embed.set_author(
+        name="{} ({})".format(after, after.id)
+    )
+    if image is not None:
+        embed.set_thumbnail(url=image)
 
+    # Finally, send the embed
+    try:
+        await userlog.send(embed=embed)
+    except discord.Forbidden:
+        pass
+    except discord.NotFound:
+        pass
+    except Exception as e:
+        client.log("Failed to post user update log for member '{}' (uid:{}) in guild '{} (gid:{})."
+                   " Exception: {}".format(after,
+                                           after.id,
+                                           after.guild.name,
+                                           after.guild.id,
+                                           e.__repr__()),
+                   context="POST_USERLOG",
+                   level=logging.WARNING)
+
+
+async def user_update_handler(client, before, after):
+    # Check the event is one we we can handle
+    if not (before.name != after.name
+            or before.avatar_url != after.avatar_url):
+        return
+    # Get the shared guilds
+    guilds = [g for g in client.guilds if after in g.members]
+
+    for guild in guilds:
+        asyncio.ensure_future(member_update_handler(client, before, after, from_user=True, guild=guild))
+
+
+@module.init_task
+def attach_userlog_handler(client):
+    client.add_after_event("member_update", member_update_handler)
+    client.add_after_event("user_update", user_update_handler)
+
+
+# Define guild configuration settings
+@module.guild_setting
+class guild_userlog(ColumnData, Channel, GuildSetting):
+    attr_name = "userlog"
+    category = "Logging"
+    read_check = None
+    write_check = guild_manager
+
+    name = "userlog"
+    desc = "Channel to log user and member metainfo updates."
+
+    long_desc = (
+        "Channel to log member and user updates, for example avatar, name, and role updates.\n"
+        "See the `userlog_events` setting for the loggable events, "
+        "and the `userlog_ignores` setting to ignore specific users."
+    )
+
+    _table_interface_name = "guild_userupdate_channel"
+    _data_column = "channelid"
+    _delete_on_none = True
+
+
+@module.guild_setting
+class guild_userlog_ignores(ListData, MemberList, GuildSetting):
+    attr_name = "userlog_ignores"
+    category = "Logging"
+    read_check = None
+    write_check = guild_manager
+
+    name = "userlog_ignores"
+    desc = "The users to ignore in the userlog."
+
+    long_desc = (
+        "Users listed in this setting will not be shown in the userlog.\n"
+        "This may be useful to e.g. ignore continuously updating bot avatars."
+    )
+
+    _table_interface_name = "guild_userupdate_ignores"
+    _data_column = "userid"
+
+
+class _userlog_event(IntegerEnum):
+    _enum = UserLogEvent
+
+    _output_map = {
+        UserLogEvent.USERNAME: "Username",
+        UserLogEvent.NICKNAME: "Nickname",
+        UserLogEvent.AVATAR: "Avatar",
+        UserLogEvent.ROLES: "Roles"
+    }
+
+
+@module.guild_setting
+class guild_userlog_events(ListData, SettingList, GuildSetting):
+    attr_name = "userlog_events"
+    category = "Logging"
+    read_check = None
+    write_check = guild_manager
+
+    name = "userlog_events"
+    desc = "The event types to log in the userlog."
+    long_desc = (
+        "The event types to be logged into the userlog."
+    )
+
+    accepts = "Comma separated list of userlog event types (listed below)."
+    _setting = _userlog_event
+    _force_unique = True
+
+    _default = [0, 1, 2, 3]
+    _table_interface_name = "guild_userupdate_events"
+    _data_column = "event"
+
+    @property
+    def embed(self):
+        embed = super().embed
+        event_types = {
+            "Username": "Updates to a member's global username.",
+            "Avatar": "Updates to a member's global avatar.",
+            "Nickname": "Updates to a member's guild nickname.",
+            "Roles": "New or removed roles for a member.",
+        }
+        table = prop_tabulate(*zip(*event_types.items()))
+        embed.add_field(
+            name="Userlog Event types",
+            value=table
+        )
+        return embed
+
+
+# Define data schemas
 channel_schema_info = schema_generator(
     "guild_userupdate_channel",
     Column('app', ColumnType.SHORTSTRING, primary=True, required=True),
@@ -117,7 +278,7 @@ def attach_userlog_data(client):
     mysql_schema, sqlite_schema, columns = ignores_schema_info
     prefix_interface = tableInterface(
         client.data,
-        "guild_userupdate_events",
+        "guild_userupdate_ignores",
         app=client.app,
         column_data=columns,
         shared=False,
