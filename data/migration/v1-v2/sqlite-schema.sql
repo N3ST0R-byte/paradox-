a4e9ccca68c2357eaf02a37100c99d52d19be557
Puerosola <pue@puerosola.pw>
2020-12-22 13:41:44 +0200

    fix (v1v2 migration): Repair migration scripts.
    Rebuild migration scripts.
    (Migration scripts were damaged by a faulty merge.)

--- data/migration/v1-v2/sqlite-schema.sql
+++ data/migration/v1-v2/sqlite-schema.sql
@@ -1,27 +1,6 @@
 INSERT INTO VERSION (version, updated_by) VALUES (2, 'v1-v2 Migration');
 
 
-CREATE TABLE guild_starboards(
-	app TEXT NOT NULL,
-	guildid INTEGER NOT NULL,
-	channelid INTEGER,
-	emoji TEXT,
-	threshold INTEGER NOT NULL DEFAULT 1,
-
-CREATE TABLE guild_starboard_roles(
-	app TEXT NOT NULL,
-	guildid INTEGER NOT NULL,
-	roleid INTEGER NOT NULL,
-
-CREATE TABLE message_stars(
-	app TEXT NOT NULL,
-	msgid INTEGER NOT NULL,
-	starmsgid INTEGER NOT NULL,
-	_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY (app,msgid)
-
-
-
 CREATE TABLE member_traffic(
 	guildid INTEGER NOT NULL,
 	userid INTEGER NOT NULL,
@@ -50,6 +29,7 @@ CREATE TABLE guild_logging_departures(
 	PRIMARY KEY (app,guildid)
 );
 
+
 CREATE TABLE guild_userupdate_channel(
 	app TEXT NOT NULL,
 	guildid INTEGER NOT NULL,
@@ -72,4 +52,33 @@ CREATE TABLE guild_userupdate_ignores(
 	userid INTEGER NOT NULL,
 	_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (app,guildid,userid)
+);
+
+
+INSERT INTO VERSION (version, updated_by) VALUES (2, 'v1-v2 Migration');
+
+CREATE TABLE guild_starboards(
+	app TEXT NOT NULL,
+	guildid INTEGER NOT NULL,
+	channelid INTEGER,
+	emoji TEXT,
+	threshold INTEGER NOT NULL DEFAULT 1,
+	_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (app,guildid)
+);
+
+CREATE TABLE guild_starboard_roles(
+	app TEXT NOT NULL,
+	guildid INTEGER NOT NULL,
+	roleid INTEGER NOT NULL,
+	_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (app,guildid)
+);
+
+CREATE TABLE message_stars(
+	app TEXT NOT NULL,
+	msgid INTEGER NOT NULL,
+	starmsgid INTEGER NOT NULL,
+	_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (app,msgid)
 );
